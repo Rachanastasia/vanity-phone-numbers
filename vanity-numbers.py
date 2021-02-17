75c4c4b436f042e0058c744b57d4f0d58242901e
Rachel <r.a.reill18@gmail.com>
2021-02-17 16:07:42 -0600

    changed set to list

--- vanity-numbers.py
+++ vanity-numbers.py
@@ -1,32 +1,47 @@
 hash = {
     '0': None,
     '1': None,
-    '2': (['a', 'b', 'c']),
-    '3': (['d', 'e', 'f']),
-    '4': (['g', 'h', 'i']),
-    '5': (['j', 'k', 'l']),
-    '6': (['m', 'n', 'o']),
-    '7': (['p', 'q', 'r', 's']),
-    '8': (['t', 'u', 'v']),
-    '9': (['w', 'x', 'y', 'z'])
+    '2': (['a', 'b', 'c', '2']),
+    '3': (['d', 'e', 'f', '3']),
+    '4': (['g', 'h', 'i', '4']),
+    '5': (['j', 'k', 'l', '5']),
+    '6': (['m', 'n', 'o', '6']),
+    '7': (['p', 'q', 'r', 's', '7']),
+    '8': (['t', 'u', 'v', '8']),
+    '9': (['w', 'x', 'y', 'z', '9'])
 }
 
-def find_all_wildcard_chars(num, i):
+def make_items_new_list_items(list, num):
+    #return set
+    #loop through set and add to set
+    #remove item from set
+    #duplicate base item
+    
+
+    #return all values in set
+
+def change_list_items(list, add_char):
+    for i in range(0, len(list)): 
+        list[i] = list[i] + add_char
+    return list
+
 
 
 def find_vanity_numbers(num, word):
-    combinations = new set() #unique values, no k:v pairs
+    combos = [] #different letter combinations
 
 #make set to return all valid letters 
 
     i=0 #idx for letters in words
     for n in num: 
-        letter = hash[n]
-        print(len(word))
-        if i+1 == len(word): 
-            combinations.add
-            print('LIMIT REACHED')
-        elif word[i] in letter: i+=1
+        if i >= len(word):
+            # new_list =  
+            print('HANDLE THIS')
+        elif word[i] in hash[n]:
+            combos = change_list_items(combos, word[i])
+            i+=1 #handle case when i > len
+        else: 
+            #return a list of matching strings
 
 
 
